9fd8b15260784e5f87e00c64337568bbe3751ebf
han <123@qq.com>
2023-04-16 23:32:15 +0800

    third commit

--- git-test/src/main/java/com/bjpowernode/Test.java
+++ git-test/src/main/java/com/bjpowernode/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("nihao1");
         System.out.println("nihao2");
+        System.out.println("nihao3");
     }
 }
